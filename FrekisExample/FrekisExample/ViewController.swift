8c278114a7a95d8909417a17420174c8ef01c3ab
Bhargav <bhavesh.iblazing@gmail.com>
2020-07-22 18:13:19 +0530

    Added test cases

---
+++ FrekisExample/FrekisExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FrekisExample
+//
+//  Created by Bhargav on 22/07/20.
+//  Copyright © 2020 Yash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
